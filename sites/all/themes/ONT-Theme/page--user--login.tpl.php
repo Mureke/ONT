b87b6866e8ef6e2d6e579f715644bd47316c5eaf
Markus <markus.suomalainen@gmail.com>
2015-11-10 16:55:19 +0200

    Login page template

---
+++ sites/all/themes/ONT-Theme/page--user--login.tpl.php
@@ -0,0 +1,115 @@
+<!-- #header -->
+<div id="header">
+	<!-- #header-inside -->
+    <div id="header-inside" class="container_12 clearfix">
+    	<!-- #header-inside-left -->
+        <div id="header-inside-left" class="grid_4">
+           
+           <div class="default-logo"> 
+                <?php if ($logo): ?>
+                <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+                <?php endif; ?>
+            </div><!-- EOF: .default-logo -->
+
+             <div class="mobile-logo">
+                <?php if ($logo): ?>
+                <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+                <?php endif; ?>
+            </div><!-- EOF: .mobile-logo -->
+
+        </div><!-- EOF: #header-inside-left -->
+  
+        <!-- #header-inside-right -->    
+        <div id="header-inside-right" class="grid_4">
+             <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('secondary-menu', 'links', 'clearfix')))); ?>
+			<?php print render($page['search_area']); ?>
+
+             <!-- MAIN NAVIGATION -->  
+        <div id="header-menu">
+              <!-- #header-menu-inside -->
+        <div id="header-menu-inside" class="clearfix"> 
+           
+         </div><!-- EOF: #header-menu-inside -->
+
+    </div><!-- EOF: #header-menu -->
+
+        </div><!-- EOF: #header-inside-right -->
+    
+    </div><!-- EOF: #header-inside -->
+
+</div><!-- EOF: #header -->
+
+
+<!-- #content -->
+<div id="content">
+	<!-- #content-inside -->
+    <div id="content-inside" class="container_12 clearfix">
+    
+        <?php if ($page['sidebar_first']) :?>
+        <!-- #sidebar-first -->
+        <div id="sidebar-first" class="grid_4">
+        	<?php print render($page['sidebar_first']); ?>
+        </div><!-- EOF: #sidebar-first -->
+        <?php endif; ?>
+        
+        <?php if ($page['sidebar_first'] && $page['sidebar_second']) { ?>
+        <div class="grid_4">
+        <?php } elseif ($page['sidebar_first'] || $page['sidebar_second']) { ?>
+        <div id="main" class="grid_8">
+		<?php } else { ?>
+        <div id="main" class="grid_12">    
+        <?php } ?>
+            
+           
+            <h1>Kirjaudu sisään</h1>
+       
+            <?php print render($title_suffix); ?>
+            
+            <?php print render($page['content']); ?>
+            
+            <?php print $feed_icons; ?>
+            
+        </div><!-- EOF: #main -->
+        
+        <?php if ($page['sidebar_second']) :?>
+        <!-- #sidebar-second -->
+        <div id="sidebar-second" class="grid_4">
+        	<?php print render($page['sidebar_second']); ?>
+        </div><!-- EOF: #sidebar-second -->
+        <?php endif; ?>  
+
+    </div><!-- EOF: #content-inside -->
+
+</div><!-- EOF: #content -->
+
+<!-- #footer-bottom -->    
+<div id="footer-bottom">
+
+	<!-- #footer-bottom-inside --> 
+    <div id="footer-bottom-inside" class="container_12 clearfix">
+    	<!-- #footer-bottom-left --> 
+    	<div id="footer-bottom-left" class="grid_8">
+            
+            <?php print render($page['footer']); ?>
+            
+        </div>
+    	<!-- #footer-bottom-right --> 
+        <div id="footer-bottom-right" class="grid_4">
+        
+        	<?php print render($page['footer_bottom_right']); ?>
+        
+        </div><!-- EOF: #footer-bottom-right -->
+       
+    </div><!-- EOF: #footer-bottom-inside -->
+    
+    <?php if (theme_get_setting('credits_display','ONT')): ?>
+    <!-- #credits -->   
+    <div id="credits" class="container_12 clearfix">
+        <div class="grid_12">
+       
+        </div>
+    </div>
+    <!-- EOF: #credits -->
+    <?php endif; ?>
+
+</div><!-- EOF: #footer -->
