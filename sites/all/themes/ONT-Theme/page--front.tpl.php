352309efdf59c429c3d5fb9634abc3920349a9a2
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-03-27 10:49:13 +0300

    Updated date module and added some css

--- sites/all/themes/ONT-Theme/page--front.tpl.php
+++ sites/all/themes/ONT-Theme/page--front.tpl.php
@@ -97,7 +97,15 @@
             <h2> Tietoa palvelusta </h2>
             
             <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
-            <p id="tietoa-front"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu. Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit. Sed augue orci, lacinia eu tincidunt et eleifend nec lacus. Donec ultricies nisl ut felis, suspendisse potenti. Lorem ipsum ligula ut hendrerit mollis, ipsum erat vehicula risus, eu suscipit sem libero nec erat. Aliquam erat volutpat. Sed congue augue vitae neque. Nulla consectetuer porttitor pede. Fusce purus morbi tortor magna condimentum vel, placerat id blandit sit amet tortor. </p>
+            <p id="tietoa-front"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem.
+             Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien.
+              Mauris varius diam vitae arcu.
+            Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit.
+             Sed augue orci, lacinia eu tincidunt et eleifend nec lacus. Donec ultricies nisl ut felis,
+              suspendisse potenti. Lorem ipsum ligula ut hendrerit mollis, ipsum erat vehicula risus, eu suscipit sem 
+              libero nec erat. Aliquam erat volutpat. Sed congue augue vitae neque.
+               Nulla consectetuer porttitor pede. Fusce purus morbi tortor magna condimentum vel, 
+               placerat id blandit sit amet tortor. </p>
             <?php print render($page['content']); ?>
             
             <?php print $feed_icons; ?>
