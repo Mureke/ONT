4b6e32c393a1c7bb7c40c78757392553ea4d56f6
Markus Suomalainen <markus.suomalainen@gmail.com>
2016-08-07 11:32:31 +0300

    Contact page layout chages

--- sites/all/themes/ONT-Theme/views-view-fields--yhteystiedot.tpl.php
+++ sites/all/themes/ONT-Theme/views-view-fields--yhteystiedot.tpl.php
@@ -3,10 +3,10 @@
 		<?php print $fields['field_profile_image'] ->content; ?>
 	</div>
 	<div id="profileInfo">
-		<?php print $fields['field_profile_name'] ->content; ?>
-		<?php print $fields['field_profile_worktitle'] ->content; ?>
-		<?php print $fields['field_profile_organization'] ->content; ?>	
-		<?php print $fields['field_profile_email'] ->content; ?>
-		<?php print $fields['field_profile_phone'] ->content; ?>
-	</div>				
+		<div class="profile-field"><strong>Nimi: </strong><?php print $fields['field_profile_name'] ->content; ?></div>
+		<div class="profile-field"><strong>Tehtävä: </strong><?php print $fields['field_profile_worktitle'] ->content; ?></div>
+		<div class="profile-field"><strong>Organisaatio: </strong><?php print $fields['field_profile_organization'] ->content; ?>	</div>
+		<div class="profile-field"><strong>Sähköposti: </strong><?php print $fields['field_profile_email'] ->content; ?></div>
+		<div class="profile-field"><strong>Puhelin: </strong><?php print $fields['field_profile_phone'] ->content; ?></div>
+</div>				
 </div>
